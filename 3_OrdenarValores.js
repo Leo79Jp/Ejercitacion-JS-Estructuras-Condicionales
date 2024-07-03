90e19a5e5e0ecdf8a95ded17ac4382b9ca74361d
Leo79Jp <leojparodi@gmail.com>
2024-07-03 18:31:49 -0300

    Complete activiad 3 y 5

--- 3_OrdenarValores.js
+++ 3_OrdenarValores.js
@@ -1,1 +1,25 @@
 // 3. Ordenar en forma creciente tres valores diferentes A, B, C.
+
+const A = 3
+const B = 2
+const C = 1
+
+
+if (A>B && A>C && B>C){
+    console.log('Se da la primera condición!');
+    console.log('Variable C = ' + C);
+    console.log('Variable B = ' + B);
+    console.log('Variable A = ' + A);
+} else if (B>A && B>C && A>C){
+    console.log('Se da la segunda condición!');
+    console.log('Variable C = ' + C);
+    console.log('Variable A = ' + A);
+    console.log('Variable B = ' + B);
+}else{
+    console.log('Se da la tercera condición!');
+    console.log('Variable B = ' + B);
+    console.log('Variable A = ' + A);
+    console.log('Variable C = ' + C);
+}
+
+
--- 5_FechaNombreMes.js
+++ 5_FechaNombreMes.js
@@ -1,3 +1,64 @@
-// 5. Leer un valor de Día, Mes y Año de una fecha (ej.: 10-5-2003). Mostrar la fecha con el respectivo nombre del mes (ej.: 10-mayo-2003). 
-// Si el número leído para el mes no está entre 1 y 12 imprimir un mensaje de error. 
+// 5. Leer un valor de Día, Mes y Año de una fecha (ej.: 10-5-2003). Mostrar la fecha con el respectivo nombre del mes (ej.: 10-mayo-2003).
+// Si el número leído para el mes no está entre 1 y 12 imprimir un mensaje de error.
 // Comprobar que el día se encuentre entre 1 y 31 y el año sea mayor que 2000.
+
+// const fecha = new Date("6-4-2024");
+// const año = fecha.getFullYear();
+// const mes = fecha.getMonth() + 1;
+// const dia = fecha.getDate();
+
+// const fecha= '7-4-2024'
+const dia = 31
+const mes = 7
+const año = 2022
+
+if (dia < 1 || dia > 31) {
+  console.log("Error!! el dia esta fuera de rango");
+} else if (año < 2000) {
+  console.log("Error!! el año es menor a 2000");
+} else {
+  let nombreMes;
+  switch (mes) {
+    case 1:
+      nombreMes = "Enero";
+      break;
+    case 2:
+      nombreMes = "Febrero";
+      break;
+    case 3:
+      nombreMes = "Marzo";
+      break;
+    case 4:
+      nombreMes = "Abril";
+      break;
+    case 5:
+      nombreMes = "Mayo";
+      break;
+    case 6:
+      nombreMes = "Junio";
+      break;
+    case 7:
+      nombreMes = "Julio";
+      break;
+    case 8:
+      nombreMes = "Agosto";
+      break;
+    case 9:
+      nombreMes = "Septiembre";
+      break;
+    case 10:
+      nombreMes = "Octubre";
+      break;
+    case 11:
+      nombreMes = "Noviembre";
+      break;
+    case 12:
+      nombreMes = "Diciembre";
+      break;
+
+    default:
+      nombreMes = "El mes no existe!";
+      break;
+  }
+  console.log(dia + "-" + nombreMes + "-" + año);
+}

5a4d02888362a698f1cc4bc475bbfe7e0755a418
Leo79Jp <leojparodi@gmail.com>
2024-07-07 19:20:57 -0300

    Modifique el ejercicio 6, agrgegue el atributo || al bloque if

--- 6_SignoZodiacal.js
+++ 6_SignoZodiacal.js
@@ -20,100 +20,52 @@ const año = '2024'
 let diaNacimiento = parseInt(prompt('Ingrese dia de nacimiento: ')); 
 let mesNacimiento = parseInt(prompt('Ingrese mes de nacimiento: ')); 
 
-if (diaNacimiento >= '20' && mesNacimiento == '1'){
+if (diaNacimiento >= '20' && mesNacimiento == '1' || diaNacimiento <= '18' && mesNacimiento == '2'){
     console.log('Su signo es Acuario');
-}else{
-    if (diaNacimiento <= '18' && mesNacimiento == '2'){
-        console.log('Su signo es Acuario');
-    }
 }
 /////////
-if (diaNacimiento >= '19' && mesNacimiento == '2'){
+if (diaNacimiento >= '19' && mesNacimiento == '2'|| diaNacimiento <= '20' && mesNacimiento == '3'){
     console.log('Su signo es Piscis');
-}else{
-    if (diaNacimiento <= '20' && mesNacimiento == '3'){
-        console.log('Su signo es Piscis');
-    }
 }
 /////////
-if (diaNacimiento >= '21' && mesNacimiento == '3'){
+if (diaNacimiento >= '21' && mesNacimiento == '3'|| diaNacimiento <= '19' && mesNacimiento == '4'){
     console.log('Su signo es Aries');
-}else{
-    if (diaNacimiento <= '19' && mesNacimiento == '4'){
-        console.log('Su signo es Aries');
-    }
 }
 /////////
-if (diaNacimiento >= '20' && mesNacimiento == '4'){
+if (diaNacimiento >= '20' && mesNacimiento == '4' || diaNacimiento <= '20' && mesNacimiento == '5'){
     console.log('Su signo es Tauro');
-}else{
-    if (diaNacimiento <= '20' && mesNacimiento == '5'){
-        console.log('Su signo es Tauro');
-    }
 }
 /////////
-if (diaNacimiento >= '21' && mesNacimiento == '5'){
+if (diaNacimiento >= '21' && mesNacimiento == '5' || diaNacimiento <= '20' && mesNacimiento == '6'){
     console.log('Su signo es Géminis');
-}else{
-    if (diaNacimiento <= '20' && mesNacimiento == '6'){
-        console.log('Su signo es Géminis');
-    }
 }
 /////////
-if (diaNacimiento >= '21' && mesNacimiento == '6'){
+if (diaNacimiento >= '21' && mesNacimiento == '6'|| diaNacimiento <= '22' && mesNacimiento == '7'){
     console.log('Su signo es Cáncer');
-}else{
-    if (diaNacimiento <= '22' && mesNacimiento == '7'){
-        console.log('Su signo es Cáncer');
-    }
 }
 /////////
-if (diaNacimiento >= '23' && mesNacimiento == '7'){
+if (diaNacimiento >= '23' && mesNacimiento == '7' || diaNacimiento <= '22' && mesNacimiento == '8'){
     console.log('Su signo es Leo');
-}else{
-    if (diaNacimiento <= '22' && mesNacimiento == '8'){
-        console.log('Su signo es Leo');
-    }
 }
 /////////
-if (diaNacimiento >= '23' && mesNacimiento == '8'){
+if (diaNacimiento >= '23' && mesNacimiento == '8' || diaNacimiento <= '22' && mesNacimiento == '9'){
     console.log('Su signo es Virgo');
-}else{
-    if (diaNacimiento <= '22' && mesNacimiento == '9'){
-        console.log('Su signo es Virgo');
-    }
 }
 /////////
-if (diaNacimiento >= '23' && mesNacimiento == '9'){
+if (diaNacimiento >= '23' && mesNacimiento == '9'|| diaNacimiento <= '22' && mesNacimiento == '10'){
     console.log('Su signo es Libra');
-}else{
-    if (diaNacimiento <= '22' && mesNacimiento == '10'){
-        console.log('Su signo es Libra');
-    }
 }
 /////////
-if (diaNacimiento >= '23' && mesNacimiento == '10'){
+if (diaNacimiento >= '23' && mesNacimiento == '10' || diaNacimiento <= '21' && mesNacimiento == '11'){
     console.log('Su signo es Escorpio');
-}else{
-    if (diaNacimiento <= '21' && mesNacimiento == '11'){
-        console.log('Su signo es Escorpio');
-    }
 }
 /////////
-if (diaNacimiento >= '22' && mesNacimiento == '11'){
+if (diaNacimiento >= '22' && mesNacimiento == '11' || diaNacimiento <= '21' && mesNacimiento == '12'){
     console.log('Su signo es Sagitario');
-}else{
-    if (diaNacimiento <= '21' && mesNacimiento == '12'){
-        console.log('Su signo es Sagitario');
-    }
 }
 /////////
-if (diaNacimiento >= '22' && mesNacimiento == '12'){
+if (diaNacimiento >= '22' && mesNacimiento == '12' || diaNacimiento <= '19' && mesNacimiento == '1'){
     console.log('Su signo es Capricornio');
-}else{
-    if (diaNacimiento <= '19' && mesNacimiento == '1'){
-        console.log('Su signo es Capricornio');
-    }
 }
 
 
